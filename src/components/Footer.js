07a5cdda65124f0592d0fe553f02c59f7a091cce
NemanjaMijucic <nemanja.mijuci@gmail.com>
2023-12-03 13:40:27 +0100

    animation added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,70 @@
-import { Box, Typography } from "@mui/material";
 import React from "react";
+import {
+  Typography,
+  Box,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import HomeIcon from "@mui/icons-material/Home";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
 
 const Footer = () => {
   return (
-    <Box
-      sx={{
-        backgroundColor: "#012a4a",
-        padding: "10px",
-        textAlign: "center",
-      }}
-    >
-      <Typography>copyright NemanjaMijucic</Typography>
-    </Box>
+    <>
+      <Grid>
+        <Grid item xs={12} md={12}>
+          <Box
+            sx={{
+              backgroundColor: "#012a4a",
+              color: "#fff",
+              textAlign: "center",
+              margin: "0 auto",
+            }}
+          >
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon sx={{ color: "#fff" }}>
+                    <PhoneIphoneIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="064/123-4567" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon sx={{ color: "#fff" }}>
+                    <EmailIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="borko.advokat@gmail.com" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon sx={{ color: "#fff" }}>
+                    <HomeIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="123 Main street" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          backgroundColor: "#012a4a",
+          padding: "10px",
+          textAlign: "center",
+        }}
+      >
+        <Typography>copyright NemanjaMijucic</Typography>
+      </Box>
+    </>
   );
 };
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -16,23 +16,41 @@ import Background from "./Background";
 import EmailIcon from "@mui/icons-material/Email";
 import HomeIcon from "@mui/icons-material/Home";
 import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import { useSpring, animated } from "react-spring";
+
+const AnimatedBox = animated(Box);
 
 const Main = () => {
-  const [showContactForm, setShowContactForm] = useState(false);
-  const [showAbout, setShowAbout] = useState(false);
+  const [showContactForm, setShowContactForm] = useState(true);
+  const [showAbout, setShowAbout] = useState(true);
+
+  // React Spring animation configuration
+  const aboutAnimation = useSpring({
+    opacity: showAbout ? 1 : 0,
+    from: { opacity: 0 },
+    config: { duration: 700 }, // Adjust the duration here (in milliseconds)
+  });
+
+  const contactFormAnimation = useSpring({
+    opacity: showContactForm ? 1 : 0,
+    from: { opacity: 0 },
+    config: { duration: 1000 }, // Adjust the duration here (in milliseconds)
+  });
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setShowAbout(true);
+      if (window.scrollY > 300) {
+        setTimeout(() => {
+          setShowAbout(true);
+        }, 1000);
       } else {
-        setShowAbout(false);
+        setShowAbout(true);
       }
 
-      if (window.scrollY > 100) {
+      if (window.scrollY > 800) {
         setShowContactForm(true);
       } else {
-        setShowContactForm(false);
+        setShowContactForm(true);
       }
     };
 
@@ -58,139 +76,152 @@ const Main = () => {
           >
             <Grid container spacing={4}>
               <Grid item xs={12} md={7}>
-                <Box
-                  sx={{
-                    padding: "20px",
-                    backgroundColor: "#012a4a",
-                    color: "#fff",
-                  }}
-                >
-                  <Avatar sx={{ width: 150, height: 150, margin: "0 auto" }}>
-                    <Background />
-                  </Avatar>
-                  <Typography variant="h5" align="center" mt={2}>
-                    Borko Advokat
-                  </Typography>
-                  <Typography variant="body1" align="center">
-                    Lawyer at Law Firm XYZ
-                  </Typography>
-                  <Typography variant="body2" align="justify" mt={2}>
-                    // Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Fusce // pulvinar arcu eget tincidunt porttitor. Etiam nec
-                    massa nec // mauris accumsan vulputate sit amet eu velit.
-                    Lorem ipsum dolor // sit amet, consectetur adipiscing elit.
-                    Fusce pulvinar arcu // eget tincidunt porttitor. Etiam nec
-                    massa nec mauris accumsan // vulputate sit amet eu velit.
-                    Lorem ipsum dolor sit amet, // consectetur adipiscing elit.
-                    Fusce pulvinar arcu eget // tincidunt porttitor. Etiam nec
-                    massa nec mauris accumsan // vulputate sit amet eu velit.
-                    Lorem ipsum dolor sit amet, // consectetur adipiscing elit.
-                    Fusce pulvinar arcu eget // tincidunt porttitor. Etiam nec
-                    massa nec mauris accumsan // vulputate sit amet eu velit. //{" "}
-                  </Typography>
-                </Box>
+                <AnimatedBox style={aboutAnimation}>
+                  <Box
+                    sx={{
+                      padding: "20px",
+                      backgroundColor: "#012a4a",
+                      color: "#fff",
+                      position: "relative", // Added position relative
+                      zIndex: 1, // Added zIndex
+                    }}
+                  >
+                    <Avatar sx={{ width: 150, height: 150, margin: "0 auto" }}>
+                      <Background />
+                    </Avatar>
+                    <Typography variant="h5" align="center" mt={2}>
+                      Borko Advokat
+                    </Typography>
+                    <Typography variant="body1" align="center">
+                      Lawyer at Law Firm XYZ
+                    </Typography>
+                    <Typography variant="body2" align="justify" mt={2}>
+                      // Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit. Fusce // pulvinar arcu eget tincidunt porttitor.
+                      Etiam nec massa nec // mauris accumsan vulputate sit amet
+                      eu velit. Lorem ipsum dolor // sit amet, consectetur
+                      adipiscing elit. Fusce pulvinar arcu // eget tincidunt
+                      porttitor. Etiam nec massa nec mauris accumsan //
+                      vulputate sit amet eu velit. Lorem ipsum dolor sit amet,
+                      // consectetur adipiscing elit. Fusce pulvinar arcu eget
+                      // tincidunt porttitor. Etiam nec massa nec mauris
+                      accumsan // vulputate sit amet eu velit. Lorem ipsum dolor
+                      sit amet, // consectetur adipiscing elit. Fusce pulvinar
+                      arcu eget // tincidunt porttitor. Etiam nec massa nec
+                      mauris accumsan // vulputate sit amet eu velit. //{" "}
+                    </Typography>
+                  </Box>
+                </AnimatedBox>
               </Grid>
               <Grid item xs={12} md={5}>
-                <Box
-                  mt={4}
-                  sx={{
-                    backgroundColor: "#012a4a",
-                    color: "#fff",
-                    padding: "10px",
-                  }}
-                >
-                  <Typography variant="subtitle1">
-                    Contact Information:
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    backgroundColor: "#012a4a",
-                    color: "#fff",
-                  }}
-                >
-                  <List>
-                    <ListItem disablePadding>
-                      <ListItemButton>
-                        <ListItemIcon sx={{ color: "#fff" }}>
-                          <PhoneIphoneIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="064/123-4567" />
-                      </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <ListItemButton>
-                        <ListItemIcon sx={{ color: "#fff" }}>
-                          <EmailIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="borko.advokat@gmail.com" />
-                      </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <ListItemButton>
-                        <ListItemIcon sx={{ color: "#fff" }}>
-                          <HomeIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="123 Main street" />
-                      </ListItemButton>
-                    </ListItem>
-                  </List>
-                </Box>
-                <Box
-                  mt={4}
-                  sx={{
-                    backgroundColor: "#012a4a",
-                    color: "#fff",
-                    padding: "10px",
-                  }}
-                >
-                  <Typography variant="subtitle1">Lorem Ipsum </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    backgroundColor: "#012a4a",
-                    color: "#fff",
-                  }}
-                >
-                  <List component="ul">
-                    <ListItem disablePadding>
-                      <ListItemButton>
-                        <ListItemText primary="- dolor sit amet" />
-                      </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <ListItemButton>
-                        <ListItemText primary="- consetetur elit" />
-                      </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <ListItemButton>
-                        <ListItemText primary="- vulputate sit amet" />
-                      </ListItemButton>
-                    </ListItem>
-                  </List>
-                </Box>
+                <AnimatedBox style={aboutAnimation}>
+                  <Box
+                    mt={4}
+                    sx={{
+                      backgroundColor: "#012a4a",
+                      color: "#fff",
+                      padding: "10px",
+                    }}
+                  >
+                    <Typography variant="subtitle1">
+                      Contact Information:
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      backgroundColor: "#012a4a",
+                      color: "#fff",
+                    }}
+                  >
+                    <List>
+                      <ListItem disablePadding>
+                        <ListItemButton>
+                          <ListItemIcon sx={{ color: "#fff" }}>
+                            <PhoneIphoneIcon />
+                          </ListItemIcon>
+                          <ListItemText primary="064/123-4567" />
+                        </ListItemButton>
+                      </ListItem>
+                      <ListItem disablePadding>
+                        <ListItemButton>
+                          <ListItemIcon sx={{ color: "#fff" }}>
+                            <EmailIcon />
+                          </ListItemIcon>
+                          <ListItemText primary="borko.advokat@gmail.com" />
+                        </ListItemButton>
+                      </ListItem>
+                      <ListItem disablePadding>
+                        <ListItemButton>
+                          <ListItemIcon sx={{ color: "#fff" }}>
+                            <HomeIcon />
+                          </ListItemIcon>
+                          <ListItemText primary="123 Main street" />
+                        </ListItemButton>
+                      </ListItem>
+                    </List>
+                  </Box>
+                  <Box
+                    mt={4}
+                    sx={{
+                      backgroundColor: "#012a4a",
+                      color: "#fff",
+                      padding: "10px",
+                    }}
+                  >
+                    <Typography variant="subtitle1">Lorem Ipsum </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      backgroundColor: "#012a4a",
+                      color: "#fff",
+                    }}
+                  >
+                    <List component="ul">
+                      <ListItem disablePadding>
+                        <ListItemButton>
+                          <ListItemText primary="- dolor sit amet" />
+                        </ListItemButton>
+                      </ListItem>
+                      <ListItem disablePadding>
+                        <ListItemButton>
+                          <ListItemText primary="- consetetur elit" />
+                        </ListItemButton>
+                      </ListItem>
+                      <ListItem disablePadding>
+                        <ListItemButton>
+                          <ListItemText primary="- vulputate sit amet" />
+                        </ListItemButton>
+                      </ListItem>
+                    </List>
+                  </Box>
+                </AnimatedBox>
               </Grid>
             </Grid>
             {showAbout && (
               <Grid container spacing={4}>
                 <Grid item xs={12} md={6}>
                   <Box mt={6}>
-                    <Paper
-                      sx={{ padding: "10px 20px", backgroundColor: "#2a6f97" }}
-                    >
-                      <Typography align="justify">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Fusce pulvinar arcu eget tincidunt porttitor. Etiam nec
-                        massa nec mauris accumsan vulputate sit amet eu velit.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Fusce pulvinar arcu eget tincidunt porttitor. Etiam nec
-                        massa nec mauris accumsan vulputate sit amet eu velit.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Fusce pulvinar arcu eget tincidunt porttitor. Etiam nec
-                        massa nec mauris accumsan vulputate sit amet eu velit.
-                      </Typography>
-                    </Paper>
+                    <AnimatedBox style={aboutAnimation}>
+                      <Paper
+                        sx={{
+                          padding: "10px 20px",
+                          backgroundColor: "#2a6f97",
+                        }}
+                      >
+                        <Typography align="justify">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Fusce pulvinar arcu eget tincidunt porttitor.
+                          Etiam nec massa nec mauris accumsan vulputate sit amet
+                          eu velit. Lorem ipsum dolor sit amet, consectetur
+                          adipiscing elit. Fusce pulvinar arcu eget tincidunt
+                          porttitor. Etiam nec massa nec mauris accumsan
+                          vulputate sit amet eu velit. Lorem ipsum dolor sit
+                          amet, consectetur adipiscing elit. Fusce pulvinar arcu
+                          eget tincidunt porttitor. Etiam nec massa nec mauris
+                          accumsan vulputate sit amet eu velit.
+                        </Typography>
+                      </Paper>
+                    </AnimatedBox>
                   </Box>
                 </Grid>
                 <Grid item xs={12} md={6}>
@@ -214,7 +245,11 @@ const Main = () => {
                 </Grid>
               </Grid>
             )}
-            {showContactForm && <Contact />}
+            {showContactForm && (
+              <AnimatedBox style={contactFormAnimation}>
+                <Contact />
+              </AnimatedBox>
+            )}
           </Paper>
         </Grid>
       </Grid>
